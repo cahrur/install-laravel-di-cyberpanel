7336c2c07db19c576c68ae79a674d1e70117a193
Cahrur Rozid <104527483+cahrur@users.noreply.github.com>
2024-11-03 22:12:28 +0700

    Create index.php

---
+++ public_html/index.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
+
+define('LARAVEL_START', microtime(true));
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader
+| for our application. We just need to utilize it! We'll simply require
+| it into the script here so we don't need to manually load our classes.
+|
+*/
+
+require __DIR__.'/public/index.php';
